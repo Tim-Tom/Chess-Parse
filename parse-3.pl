b311260508d2c2d4ff1c201a97cee5a894094895
Tim Bollman <Tim.Bollman@gmail.com>
2017-02-10 22:48:54 -0800

    Make it so that the static distribution somewhat evenly distributes all the files by size across the processes. Drops the runtime to 45s, which is slightly lower than the simple xargs parallel C version.

--- parse-3.pl
+++ parse-3.pl
@@ -5,14 +5,30 @@ use v5.24;
 
 my %results;
 # Simple Search
-my @files = sort { -s $a <=> -s $b } @ARGV;
+my @files = sort { -s $b <=> -s $a } @ARGV;
 
 my $NUM_PROCESSES = 4;
 
+my @process_files = map { [] } 1 .. $NUM_PROCESSES;
+my @process_sizes = map { 0 } 1 .. $NUM_PROCESSES;
+
+for my $file (@files) {
+  my $min_process = 0;
+  my $min_size = $process_sizes[0];
+  for my $p (1 .. $#process_sizes) {
+    if ($process_sizes[$p] < $min_size) {
+      $min_process = $p;
+      $min_size = $process_sizes[$p];
+    }
+  }
+  $process_sizes[$min_process] += -s $file;
+  push(@{$process_files[$min_process]}, $file);
+
+}
+
 for my $process (0 .. $NUM_PROCESSES - 1) {
   fork and next;
-  for (my $i = $process; $i < @files; $i += $NUM_PROCESSES) {
-    my $filename = $files[$i];
+  for my $filename (@{$process_files[$process]}) {
     open(my $in, '<', $filename) or die "Unable to open $filename for read: $!";
     while(<$in>) {
       next unless m!^\[Result\s+"([01/-]{2})!;
