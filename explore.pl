c9262c4f1e6c4d98e7b203ba891b1696f704c288
Tim Bollman <Tim.Bollman@gmail.com>
2017-02-10 19:39:08 -0800

    Expand exploration of format to make sure I'm not missing anything else base on case sensitivity or leading whitespace.
    For reference, at the moment the final version from his blog runs in ~1m20 on my machine
    while the naive perl version runs in ~2m20. Not sure if the slower time is due to reading
    more files or that I'm running in a local virtual machine on windows (which generally has
    disk read times about 1/3 of linux for small files from what I remember).

--- explore.pl
+++ explore.pl
@@ -9,16 +9,18 @@ my %extra;
 # Simple line by line search
 while(<ARGV>) {
   # Make sure our parsing format doesn't miss any lines.
-  die $_ if /^\[Result/ && !/^\[Result\s+"[^"]+"\]\s*$/;
+  die "$ARGV [line $.]: $_" if /^\s*\[Result/i && !/^\[Result\s+"[^"]+"\]\s*$/;
   next unless /^\[Result "([^"]+)"\]\s*$/;
   ++$results{$1};
   push(@{ $extra{$ARGV} }, $.);
 }
+exit;
 
 # Check for weird formatting
 say "$_: $results{$_}" foreach (keys %results);
 
-# Look to see if we can get away with 
+# Look to see if we can get away with only checking until we find the first result
+# instance in the file.
 foreach (grep { @{ $extra{$_} } > 1 } keys %extra) {
   say "$_: @{ $extra{$_} }";
 }
